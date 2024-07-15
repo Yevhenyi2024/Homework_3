ff5cf0f820fa15621558784a96eddeaa8a767745
Melnyk Yevhenii <karimovzena457@gmail.com>
2024-07-15 18:25:54 +0300

    HOME REMODELED_task2

--- 3_2.js
+++ 3_2.js
@@ -1,12 +1,14 @@
-const number = 1700;
+const number = 1900;
 let templist_1 =`Цей рік (${number}) високосний`;
 let templist_2 = `Цей рік (${number})не високосний`;
 let year_leap = 365.24219
 let year = 1
 function a_leap_years(num){
-if ((num % 4 === 0 && num % 100 !== 0)   || num % 400 === 0 ){
+if (num % 4 === 0 && num % 100 !== 0){
     console.log (templist_1)
-} else {
+} else if(num % 400 === 0 ){
+    console.log(templist_1)
+}else{
     console.log(templist_2)
 }
 }
