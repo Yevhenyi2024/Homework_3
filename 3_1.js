b70f20014d7331c0c8920588b8e451522ccd55c0
Melnyk Yevhenii <karimovzena457@gmail.com>
2024-07-15 17:59:08 +0300

    HOME REMODELED

--- 3_1.js
+++ 3_1.js
@@ -1,16 +1,16 @@
-const number = 15 ; 
+// const number = 15 ; 
 
-function checkNumber(num) {
-    if (num % 3 === 0 && num % 5 === 0) { 
-        console.log("FizzBuzz");
-    } else if (num % 3 === 0) {
-        console.log("Fizz");
-    } else if (num % 5 === 0) {
-        console.log("Buzz");
-    } else {
-        console.log("Число не ділиться на 3 або 5");
-    }
-}
+// function checkNumber(num) {
+//     if (num % 3 === 0 && num % 5 === 0) { 
+//         console.log("FizzBuzz");
+//     } else if (num % 3 === 0) {
+//         console.log("Fizz");
+//     } else if (num % 5 === 0) {
+//         console.log("Buzz");
+//     } else {
+//         console.log("Число не ділиться на 3 або 5");
+//     }
+// }
 
-checkNumber(number);
+// checkNumber(number);
 
--- 3_2.js
+++ 3_2.js
@@ -1,8 +1,10 @@
-const number = 2025;
+const number = 1700;
 let templist_1 =`Цей рік (${number}) високосний`;
 let templist_2 = `Цей рік (${number})не високосний`;
+let year_leap = 365.24219
+let year = 1
 function a_leap_years(num){
-if (num % 4 === 0 ) {
+if ((num % 4 === 0 && num % 100 !== 0)   || num % 400 === 0 ){
     console.log (templist_1)
 } else {
     console.log(templist_2)
